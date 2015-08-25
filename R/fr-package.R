b66913208b38020272980764d7f882b2c4fe69a7
Al-Ahmadgaid Asaad <alstated@gmail.com>
2015-08-25 23:44:26 +0800

    Added Files

---
+++ R/fr-package.R
@@ -0,0 +1,11 @@
+#' Face Recognition using Principal Component Analysis Package
+#' 
+#' @description
+#' This package uses principal component analysis for face recognition.
+#' 
+#' @author Al-Ahmadgaid B. Asaad
+#' @import Rcpp EBImage reshape2 lattice
+#' @docType package
+#' @useDynLib fr
+#' @name fr-package
+NULL
